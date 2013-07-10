c3fa73234f2f20c877303598d2a796022e119db7
Brad Chun <brad@bradchun.com>
2013-07-10 19:00:11 +0000

    updated web.js with index.html fs.readfile and buf.toString

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var buf = new Buffer;
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+	buf = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(buf.toString());
+//  response.send('Hello World2!');
 });
 
 var port = process.env.PORT || 5000;
